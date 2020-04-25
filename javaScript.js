e8fc82fb44cec7f326581d618e96ddff8c55005f
Marley <mross19@georgefox.edu>
2020-04-24 22:03:03 -0700

    added some stuff

---
+++ javaScript.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+        $("#thanks").hide();
+		$('#magazine').turn({
+			display: 'double',
+			acceleration: true,
+			autoCenter: true,
+			gradients: !$.isTouch,
+			elevation: 50,
+			when: {
+				turned: function(e, page) {
+				/*console.log('Current view: ', $(this).turn('view'));*/
+				}
+			}
+		});
+	});
+	/*checks if left or right arrow key*/
+	$(window).bind('keydown', function(e){
+		
+		if (e.keyCode==37)
+			$('#magazine').turn('previous');
+		else if (e.keyCode==39)
+			$('#magazine').turn('next');
+			
+    });
+    
+
+    /* hide contact form */
+    $("button").click(function() {
+        $("#contact").hide();
+        $("#thanks").show();
+    });
+
+
+    /* Gets information */
+	$("button").click(function(){
+		var resName = $("#response-name").val();
+		var email = $("#response-email").val();
+
+		//$("#response-area").append(r);
+		loadresponse(resName, email);
+		$("#response-name").val("");
+		$("#response-email").val("");
+		console.log("name:" + resName);
+	});
+
+	function loadresponse(response){
+		
+		var responseInput = {
+			"text": resName }
+		}
+		$.ajax({
+			url: "https://fapimail.p.rapidapi.com/email/send",
+			type: "POST",
+			data: JSON.stringify(responseInput),
+			contentType: "application/json",
+			success: displayResponse
+	});
+
+	function displayResponse(data)
+	{
+		console.log("success!"+ data);
+		for(key in data){
+		//console.log(key+" "+data[key]);
+		$("#response-area").append(key+" "+data[key]+"<br>");
+    };
+    
+    /* hide contact form */
+    $("#button").click(function() {
+        $("#contact").hide();
+        $("#thanks").show();
+    });
+	// Email web service 
+	var unirest = require("unirest");
+
+	var req = unirest("POST", "https://fapimail.p.rapidapi.com/email/send");
+
+	req.headers({
+		"x-rapidapi-host": "fapimail.p.rapidapi.com",
+		"x-rapidapi-key": "6ac5c44e24msh53d470de62b95f9p151ceejsn06ab329a114b",
+		"content-type": "application/json",
+		"accept": "application/json"
+	});
+
+	req.type("json");
+	req.send({
+		"recipient": "ravenRecievedEmail45@gmail.com",
+		"sender": "ravenRecievedEmail45@gmail.com",
+		"subject": "Subject of Email",
+		"message": "Body of Email"
+	});
+
+	req.end(function (res) {
+		if (res.error) throw new Error(res.error);
+
+		console.log(res.body);
+    });
+    
+
+};
